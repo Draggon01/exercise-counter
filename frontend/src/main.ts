4c5942c1a0d439b6c711572abc99450450be428f
Draggon <leonunterberger@gmx.at>
2026-05-07 19:49:16 +0200

    EX-45 | fix icon problem

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,3 @@
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import '@shoelace-style/shoelace/dist/shoelace.js';
 import '@shoelace-style/shoelace'
-
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
-setBasePath('/assets');
